66de6ee78d77a64bc6c937cab115c19093e1ff8f
Cordelra Lowman <125163096+CTheTechGuru@users.noreply.github.com>
2023-07-31 00:18:12 -0400

    Python Program Automation.py

--- Python Windows Program Automation/Python Program Automation.py
+++ Python Windows Program Automation/Python Program Automation.py
@@ -1,13 +1,10 @@
-## Python Web Automation Script
+## Python Program Script
 ## Steps 
-import webbrowser as wb  ##Import webbrowser
 
-def workauto(): #create function that will find the path of the browser and create a list of urls. 
-    Chrome_path = 'C:/Program Files/Google/Chrome/Application/chrome.exe %s' #Web path Variable
-    URLS = ['www.google.com',] #List of urls
+import os as launch  ## 
 
-    for url in URLS:# loop through the list which will have each tab open. 
-        wb.get(Chrome_path).open(url) #module and method to open url
+def workauto():
+    codepath = 'C:\\Program Files\\PuTTY\\putty.exe'  ### Open software
+    launch.startfile(codepath)
     
-
-workauto() # call the function 
+workauto()
